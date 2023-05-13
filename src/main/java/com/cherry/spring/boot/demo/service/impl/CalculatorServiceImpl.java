705581c4b9b02a37bb52b97c8000d5542ebe9524
olgacherry <zebroid81@yandex.ru>
2023-05-14 00:12:28 +0300

    дз

--- src/main/java/com/cherry/spring/boot/demo/service/impl/CalculatorServiceImpl.java
+++ src/main/java/com/cherry/spring/boot/demo/service/impl/CalculatorServiceImpl.java
@@ -7,22 +7,25 @@ import org.springframework.stereotype.Service;
 public class CalculatorServiceImpl implements CalculatorService {
 
     @Override
-    public int plus(int num1, int num2) {
+    public Integer plus(Integer num1, Integer num2) {
         return num1 + num2;
     }
 
     @Override
-    public int minus(int num1, int num2) {
+    public Integer minus(Integer num1, Integer num2) {
         return num1 - num2;
     }
 
     @Override
-    public int multiply(int num1, int num2) {
+    public Integer multiply(Integer num1, Integer num2) {
         return num1 * num2;
     }
 
     @Override
-    public int divide(int num1, int num2) {
+    public Integer divide(Integer num1, Integer num2) {
+        if(num2==0){
+            throw new IllegalArgumentException();
+        }
         return num1 / num2;
     }
 }
